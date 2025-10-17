4aa286b05b929c8a676218c031c63562474a55c1
Sergio Semedi <sergio.semedi@gmail.com>
2025-10-17 13:35:14 +0200

    improve keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,8 +1,9 @@
 local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
 
-
+---------
 -- Basics
+---------
 map("n", "<C-s>", ":w<CR>", opts)
 map("i", "jk", "<ESC>", opts)
 
@@ -34,10 +35,85 @@ map("t", "<C-j>", "<C-\\><C-n><C-w>j", opts)
 map("t", "<C-k>", "<C-\\><C-n><C-w>k", opts)
 map("t", "<C-l>", "<C-\\><C-n><C-w>l", opts)
 
+-- =============================================================================
+-- DIAGNOSTIC KEYMAPS (SHOW/HIDE/NAVIGATE)
+-- =============================================================================
 
+-- Show diagnostics for current line in floating window
+map("n", "<leader>dl", function()
+  vim.diagnostic.open_float(nil, {
+    scope = "line",
+    focusable = false,
+    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+    border = "rounded",
+    source = "if_many",
+    prefix = " ",
+  })
+end, opts)
 
+-- Show diagnostics for current cursor position
+map("n", "<leader>dc", function()
+  vim.diagnostic.open_float(nil, {
+    scope = "cursor",
+    focusable = false,
+    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+    border = "rounded",
+    source = "if_many",
+    prefix = " ",
+  })
+end, opts)
 
+-- Show all buffer diagnostics in floating window
+map("n", "<leader>db", function()
+  vim.diagnostic.open_float(nil, {
+    scope = "buffer",
+    focusable = true,
+    border = "rounded",
+    source = "if_many",
+    prefix = " ",
+  })
+end, opts)
+
+-- Toggle diagnostics visibility globally
+local diagnostics_visible = true
+map("n", "<leader>dt", function()
+  diagnostics_visible = not diagnostics_visible
+  if diagnostics_visible then
+    vim.diagnostic.enable()
+    print("Diagnostics enabled")
+  else
+    vim.diagnostic.enable(false)
+    print("Diagnostics disabled")
+  end
+end, opts)
+
+-- Toggle virtual text (inline diagnostics)
+map("n", "<leader>dv", function()
+  local current_config = vim.diagnostic.config()
+  vim.diagnostic.config({
+    virtual_text = not current_config.virtual_text,
+  })
+  print("Virtual text: " .. tostring(not current_config.virtual_text))
+end, opts)
+
+-- Navigate diagnostics
+map("n", "[d", vim.diagnostic.goto_prev, opts)
+map("n", "]d", vim.diagnostic.goto_next, opts)
+map("n", "[e", function()
+  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end, opts)
+map("n", "]e", function()
+  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+end, opts)
+
+-- Show diagnostics in location list or quickfix
+map("n", "<leader>dq", vim.diagnostic.setqflist, opts)
+map("n", "<leader>dd", vim.diagnostic.setloclist, opts)
+
+----------
 -- Plugins
+----------
+-- telescope
 map('n', '<leader><leader>', '<cmd>Telescope find_files<cr>', opts)
 map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
 map('n', '<leader>fb', ':Telescope buffers<CR>', opts)
@@ -47,11 +123,11 @@ map("n", "<leader>fp", ":Telescope projects<CR>")
 map("n", "<leader>gs", ":Git<CR>", opts)
 
 
+-- oil .nvim
 map("n", "<leader>e", "<cmd>Oil<CR>", opts)
--- Up level dir
 map("n", "-", "<cmd>Oil<CR>", opts)
 
--- LSP keymaps
+-- lsp
 map('n', 'gd', vim.lsp.buf.definition, opts)
 map('n', 'gD', vim.lsp.buf.declaration, opts)
 map('n', 'gi', vim.lsp.buf.implementation, opts)
@@ -63,7 +139,6 @@ map('n', '<leader>f', vim.lsp.buf.format, opts)
 map('i', '<C-k>', vim.lsp.buf.signature_help, opts)
 map('n', '<leader>ds', vim.lsp.buf.document_symbol, opts)
 map('n', '<leader>ws', vim.lsp.buf.workspace_symbol, opts)
-
 
 -- harpoon
 map("n", "<leader>a", function() require("harpoon.mark").add_file() end, opts)
