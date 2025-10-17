0277b5cc2d681036b71de566dee37abd8fb015ce
Sergio Semedi <sergio.semedi@gmail.com>
2025-10-17 11:19:17 +0200

    plugin config

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,8 +1,10 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
 local on_attach = function(client, bufnr)
-  -- Buffer local keymaps can go here, or globally as shown before
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 -- Add your LSP servers here
 local servers = { "pyright", "tsserver", "terraformls", "rust_analyzer" }
@@ -11,3 +13,83 @@ vim.lsp.enable('pyright')
 vim.lsp.enable('ts_ls')
 vim.lsp.enable('terraformls')
 vim.lsp.enable('rust_analyzer')
+
+-- Rust
+--vim.lsp.config('rust_analyzer', {
+--  on_attach = on_attach,
+--  capabilities = capabilities,
+--  settings = {
+--    ["rust-analyzer"] = {
+--      checkOnSave = {
+--        command = "clippy",
+--      },
+--      cargo = {
+--        allFeatures = true,
+--      },
+--    },
+--  },
+--})
+
+---- TypeScript/JavaScript
+--lspconfig.ts_ls.setup({
+--  on_attach = on_attach,
+--  capabilities = capabilities,
+--})
+--
+---- Python
+--lspconfig.pyright.setup({
+--  on_attach = on_attach,
+--  capabilities = capabilities,
+--  settings = {
+--    python = {
+--      analysis = {
+--        typeCheckingMode = "basic",
+--        autoSearchPaths = true,
+--        useLibraryCodeForTypes = true,
+--      },
+--    },
+--  },
+--})
+--
+---- C/C++
+--lspconfig.clangd.setup({
+--  on_attach = on_attach,
+--  capabilities = capabilities,
+--  cmd = {
+--    "clangd",
+--    "--background-index",
+--    "--clang-tidy",
+--    "--header-insertion=iwyu",
+--    "--completion-style=detailed",
+--    "--function-arg-placeholders",
+--  },
+--})
+--
+---- Bash
+--lspconfig.bashls.setup({
+--  on_attach = on_attach,
+--  capabilities = capabilities,
+--})
+--
+---- Lua (for Neovim config development)
+--lspconfig.lua_ls.setup({
+--  on_attach = on_attach,
+--  capabilities = capabilities,
+--  settings = {
+--    Lua = {
+--      runtime = {
+--        version = "LuaJIT",
+--      },
+--      diagnostics = {
+--        globals = { "vim" },
+--      },
+--      workspace = {
+--        library = vim.api.nvim_get_runtime_file("", true),
+--        checkThirdParty = false,
+--      },
+--      telemetry = {
+--        enable = false,
+--      },
+--    },
+--  },
+--})
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -15,16 +15,105 @@ require("lazy").setup({
   -- Treesitter for enhanced syntax highlighting and indenting
   {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
+    build = ":TSUpdate",
   },
 
   -- Built-in LSP configuration
   "neovim/nvim-lspconfig",
+  -- Completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",     -- LSP source
+      "hrsh7th/cmp-buffer",       -- Buffer words source
+      "hrsh7th/cmp-path",         -- File path source
+      "L3MON4D3/LuaSnip",         -- Snippet engine
+      "saadparwaiz1/cmp_luasnip", -- Snippet source
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer", keyword_length = 3 },
+          { name = "path" },
+        }),
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              luasnip = "[Snip]",
+              buffer = "[Buf]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+      })
+    end,
+  },
 
   -- Telescope for fuzzy finding with dependency plenary
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {"nvim-lua/plenary.nvim"},
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {".git/", ".cache", "%.o", "%.a", "%.out", "%.class", "%.pdf", "%.mkv", "%.mp4", "%.zip"},
+        },
+        pickers = {
+          find_files = {
+            hidden = true
+          }
+        }
+      })
+    end,
+  },
+
+  {
+    "nvim-mini/mini.comment",
+    event = "VeryLazy",
+    opts = {},
   },
 
   -- Fugitive for Git integration with lazy load on commands
